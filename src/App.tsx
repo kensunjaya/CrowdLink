a87fe5618daff32a7c02f990fcf26b03b0a3fd06
kensunjaya <kennethsunjaya@gmail.com>
2024-08-21 15:53:19 +0800

    auth functionality done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { idlFactory, canisterId } from './declarations/backend'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Actor, HttpAgent } from '@dfinity/agent';
 import { useQueryCall, useUpdateCall } from '@ic-reactor/react';
 
@@ -15,6 +16,7 @@ function App() {
   const [email, setEmail] = useState<string>('');
   const [users, setUsers] = useState<Users[]>([]);
   const [password, setPassword] = useState<string>('');
+  const [isLoginPage, setIsLoginPage] = useState<boolean>(false);
 
   const agent = new HttpAgent({ host: 'http://localhost:4943' });
   agent.fetchRootKey(); // Remove this in production
@@ -27,6 +29,21 @@ function App() {
   async function readUser(userId: any) {
     const user = await canister.read(userId);
     console.log('User data:', user);
+  }
+
+  async function getUserByEmail(email: string) {
+    const user = await canister.read(email) as any;
+    console.log(user);
+    return user[0];
+  }
+
+  const handleSignIn = async (email: string, password: string) => {
+    const user = await getUserByEmail(email) as Users;
+    if (user.password === password) {
+      alert('Login success');
+    } else {
+      alert('Wrong password');
+    }
   }
 
   async function readAllUser() {
@@ -43,37 +60,33 @@ function App() {
   }
 
   useEffect(() => {
-    // idlFactory.readAll().then((greeting: any) => {
-    //   console.log(greeting);
-    //   setGreeting(greeting);
-    // });
     readUser(0);
   }, []);
 
-  // const { data: count, call: refetchCount } = useQueryCall({
-  //   functionName: 'get',
-  // });
-
-  // const { call: increment, loading } = useUpdateCall({
-  //   functionName: 'inc',
-  //   onSuccess: () => {
-  //     refetchCount();
-  //   },
-  // });
-
   return (
     <div className="w-full min-h-screen bg-white">
-      <div className="flex flex-col space-y-2">
-        <input placeholder="username" className="py-1 px-3 border border-black rounded-md" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        <input placeholder="email" className="py-1 px-3 border border-black rounded-md" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <input placeholder="password" className="py-1 px-3 border border-black rounded-md" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button className="bg-black text-white w-fit p-2 rounded-lg" onClick={() => createUser()}>Submit</button>
+      <button className="bg-black text-white w-fit p-2 rounded-lg mb-5" onClick={() => setIsLoginPage(!isLoginPage)}>Switch mode</button>
+      <div className="flex flex-col space-y-3">
+        {isLoginPage && (
+          <>
+            <input placeholder="email" className="py-1 px-3 border border-black rounded-md" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input placeholder="password" className="py-1 px-3 border border-black rounded-md" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button className="bg-black text-white w-fit p-2 rounded-lg" onClick={() => handleSignIn(email, password)}>Sign In</button>
+          </>
+        )}
+        {!isLoginPage && (
+          <>
+            <input placeholder="username" className="py-1 px-3 border border-black rounded-md" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input placeholder="email" className="py-1 px-3 border border-black rounded-md" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input placeholder="password" className="py-1 px-3 border border-black rounded-md" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button className="bg-black text-white w-fit p-2 rounded-lg" onClick={() => createUser()}>Sign Up</button>
+          </>
+        )}
         <button className="bg-black text-white w-fit p-2 rounded-lg" onClick={() => readAllUser()}>Refetch users</button>
+        {/* <button className="bg-black text-white w-fit p-2 rounded-lg" onClick={() => getUserByEmail(email)}>Test button</button> */}
+
       </div>
       <div className="card">
-        {/* <button onClick={increment} disabled={loading}>
-          count is {count?.toString() ?? 'loading...'}
-        </button> */}
         {users.map((value: any, index: number) => {
           return (
             <div key={index}>
